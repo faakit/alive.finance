fd13cbc32e47732e05bc098bb5e0fcb637344b5e
faakit <64740422+faakit@users.noreply.github.com>
2022-04-05 13:35:14 -0300

    fix: Fixed stockInput component way to handle form submiting

--- frontend/src/components/StockInput.tsx
+++ frontend/src/components/StockInput.tsx
@@ -1,16 +1,24 @@
 import { Flex, FormControl, Icon, Input, InputGroup, InputGroupProps, InputLeftElement } from "@chakra-ui/react";
-import { FormEventHandler } from "react";
+import React, { useState } from "react";
 import { RiAddFill } from "react-icons/ri";
 
 interface StockInputProps extends InputGroupProps { 
-    onSubmit: FormEventHandler;
+    handleSubmit: (stock: string) => void;
 }
 
-export function StockInput({ onSubmit, ...rest }: StockInputProps) {
+export function StockInput({ handleSubmit, ...rest }: StockInputProps) {
+    const [stock, setStock] = useState('');
+
+    function submitHandler(ev: React.FormEvent){
+        ev.preventDefault();
+
+        handleSubmit(stock);
+    }
+
     return (
         <Flex 
         as="form"
-        onSubmit={onSubmit}
+        onSubmit={(ev) => submitHandler(ev)}
         flex="1"
         >
             <FormControl>
@@ -25,7 +33,13 @@ export function StockInput({ onSubmit, ...rest }: StockInputProps) {
                     >
                         <Icon as={RiAddFill} fontSize="1.5rem" />
                     </InputLeftElement>
-                    <Input placeholder='Adicionar ação' h="3rem" fontSize="1.5rem" />
+                    <Input 
+                    id="stock" 
+                    placeholder='Adicionar ação' 
+                    h="3rem" 
+                    fontSize="1.5rem" 
+                    onChange={(ev) => setStock(ev.target.value)}
+                    />
                 </InputGroup>
             </FormControl>
         </Flex>
--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,15 +1,20 @@
 import { Flex } from "@chakra-ui/react"
 import { NextPage } from "next"
+import React from "react"
 import { DashboardStock } from "../components/DashboardStock"
 import { Header } from "../components/Header"
 import { StockInput } from "../components/StockInput"
 
 const Dashboard: NextPage = () => {
+    function handleSubmit(stock: string) {
+        console.log(stock);
+    }
+
     return (
         <Flex w={"80%"} margin="0 auto" direction="column">
             <Header />
 
-            <StockInput mt='2rem' onSubmit={() => alert("Enviado")}/>
+            <StockInput mt='2rem' handleSubmit={(stock) => handleSubmit(stock)}/>
 
             <Flex wrap="wrap" gap={6} mt="2rem" justifyContent="start">
                 <DashboardStock title={"GOOGL"}/>
