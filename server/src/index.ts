3d566e48321b195e7a75ae6d716cb2be5b3ac142
faakit <64740422+faakit@users.noreply.github.com>
2022-04-14 06:41:29 -0300

    fix: added process env port to work with heroku

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,6 @@ import app from './main/app';
 
 assert.equal(process.env.TZ, 'utc');
 
-app.listen(3001, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log('Server Running...');
 });
